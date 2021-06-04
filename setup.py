d4c00d46739b95876af7e55f1b51ded9f7ec56f3
Bradley Nobbs <bradleynobbs@macbook-air.mynet>
2021-06-04 15:04:53 +0100

    adding installation guide

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="faber", # Replace with your own username
+    version="0.0.1",
+    author="Bradley Nobbs",
+    author_email="bradleynobbs@gmail.com",
+    description="Quick start ml experimentation pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    entry_points ={
+        'console_scripts': [
+            'faber = faber.cli:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
