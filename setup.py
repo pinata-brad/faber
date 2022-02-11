eb5a50de5e2399fc5abea131e783478b9b994af1
brad-lw <92023888+brad-lw@users.noreply.github.com>
2022-02-11 10:43:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="faber", 
-    version="0.0.3",
+    version="0.0.4",
     author="Bradley Nobbs",
     author_email="bradleynobbs@gmail.com",
     description="Quick start ml experimentation pipelines",
